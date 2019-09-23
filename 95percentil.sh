abe7ef539582a761fcc9812c5172b4d50c11a7b5
André Dias <35349583+andrediashexa@users.noreply.github.com>
2019-09-23 17:58:49 -0300

    ADD the 95current

--- 95percentil.sh
+++ 95percentil.sh
@@ -6,21 +6,29 @@ table=zabbix
 nowmonth=$(date +%Y-%m)
 month=$(date -d "$nowmonth-15 last month" '+%m')
 year=$(date -d "$nowmonth-15 last month" '+%Y')
-dir=/tmp/teste
+currmonth=$(date +%m)
+curryear=$(date +%Y)
+touch /tmp/lastmonth-$2
+LASTMONTH=/tmp/lastmonth-$2
+CURRENTMONTH=/tmp/currentmonth-$2
 
 
-mysql -u$user -p$password $table -e "select FROM_UNIXTIME(clock),round(value/1000000) from history_uint where itemid = $2 and FROM_UNIXTIME (clock, '%m') = $month AND FROM_UNIXTIME (clock, '%Y') = $year order by value DESC;" 2>/dev/null > $2
-wc=$(cat $tmpdir | wc | awk {'print $1'})
+mysql -u$user -p$password $table -e "select FROM_UNIXTIME(clock),round(value/1000000) from history_uint where itemid = $2 and FROM_UNIXTIME (clock, '%m') = $month AND FROM_UNIXTIME (clock, '%Y') = $year order by value DESC;" 2>/dev/null > $LASTMONTH
+wc=$(cat $LASTMONTH | wc | awk {'print $1'})
 
 if [ $1 = "report" ]; then
-        tail -n +$(expr $wc / 100 \* 5 + 1) $2
-        rm -rf $2
+        tail -n +$(expr $wc / 100 \* 5 + 1) $LASTMONTH
         exit
 fi
 
 if [ $1 = "95" ]; then
-        sed -n $(expr $wc / 100 \* 5 + 1)p $2 | awk {'print $3'}
-        rm -rf $2
+        sed -n $(expr $wc / 100 \* 5 + 1)p $LASTMONTH | awk {'print $3'}
+        exit
+fi
+
+if [ $1 = "95current" ]; then
+        mysql -u$user -p$password $table -e "select FROM_UNIXTIME(clock),round(value/1000000) from history_uint where itemid = $2 and FROM_UNIXTIME (clock, '%m') = $currmonth AND FROM_UNIXTIME (clock, '%Y') = $curryear order by value DESC;" 2>/dev/null > $CURRENTMONTH
+        sed -n $(expr $wc / 100 \* 5 + 1)p $CURRENTMONTH | awk {'print $3'}
         exit
 fi
 
